31b3ccd606bc15b94878ed10f4a3642a18a93bff
JoseJimArg <jose.jimenez.dev@gmail.com>
2023-05-15 23:35:41 -0600

    add database configurations

--- database/database.py
+++ database/database.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://linux:131415@localhost/payments"
+
+# load the environment variables
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 try:
